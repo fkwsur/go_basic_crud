a989e4e52210909dbfd1015af4e032cce7ef436d
Hyun-Ji <guswl543210@gmail.comm>
2021-10-06 12:26:28 +0900

    first commit

---
+++ echo.go
@@ -0,0 +1,267 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
+)
+
+type RequestBody struct {
+	Message string `json:"message"`
+}
+
+type querybody struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+type Board struct {
+	gorm.Model
+	Title  string `json:"title"`
+	Desc   string `json:"desc"`
+	Writer string `json:"writer"`
+}
+
+type BoardBody struct {
+	Id    int    `json:"id"`
+	Title string `json:"title"`
+	Desc  string `json:"desc"`
+}
+
+func DBConnection() {
+	host := "root:1111@tcp(127.0.0.1:3306)/rootdb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(host), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate(&Board{})
+
+	handlerouting(db)
+
+	dbset, err := db.DB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbset.SetMaxIdleConns(0)
+	dbset.SetMaxOpenConns(5)
+	dbset.SetConnMaxLifetime(time.Hour)
+}
+
+func HelloWorld(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"result": "hello world~!!",
+	})
+}
+
+func getUser(c echo.Context) error {
+
+	id := c.Param("id")
+	return c.JSON(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.JSON(http.StatusOK, "team:"+team+", member:"+member)
+}
+
+func save(c echo.Context) error {
+
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+
+	return c.JSON(http.StatusOK, "name:"+name+", email:"+email)
+}
+
+// json
+func BodyPostTest(c echo.Context) error {
+
+	body, _ := ioutil.ReadAll(c.Request().Body)
+	req := RequestBody{}
+
+	json.Unmarshal(body, &req)
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"result": req.Message,
+	})
+}
+
+func EachQueryTest(c echo.Context) error {
+
+	first_query := c.QueryParam("name")
+	second_query, _ := strconv.Atoi(c.QueryParam("age"))
+
+	return c.JSON(http.StatusOK, querybody{
+		Name: first_query,
+		Age:  second_query,
+	})
+}
+
+func MulQueryTest(c echo.Context) error {
+	req := c.QueryParams()
+	first_query := req["name"][0]
+	second_query, _ := strconv.Atoi(req["age"][0])
+
+	return c.JSON(http.StatusOK, querybody{
+		Name: first_query,
+		Age:  second_query,
+	})
+}
+
+func ParamsTest(c echo.Context) error {
+	req := c.Param("num")
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"result": fmt.Sprintf("받은 값 : %s", req),
+	})
+}
+
+func CreatePost(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+
+		body, _ := ioutil.ReadAll(c.Request().Body)
+		req := Board{}
+		json.Unmarshal(body, &req)
+		err := db.Debug().Model(&Board{}).Create(&req).Error
+		if err != nil {
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				"result": "fail",
+			})
+		}
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"result": "success",
+		})
+	}
+}
+
+func GetPostParam(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
+		board := Board{}
+		err := db.Debug().First(&board, "id = ?", id).Error
+		//fmt.Println(rows)
+		if err != nil {
+			return c.JSON(http.StatusOK, &Board{})
+		}
+		return c.JSON(http.StatusOK, &board)
+	}
+}
+
+func GetPostQuery(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.QueryParam("id"))
+		board := Board{}
+		err := db.Debug().First(&board, "id = ?", id).Error
+		if err != nil {
+			return c.JSON(http.StatusOK, &Board{})
+		}
+		return c.JSON(http.StatusOK, &board)
+	}
+}
+
+func GetAllPost(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		page, _ := strconv.Atoi(c.QueryParam("page"))
+		offset := 0
+		if page > 1 {
+			offset = 10 * (page - 1)
+		}
+		boards := []Board{}
+		err := db.Debug().Model(&Board{}).Limit(10).Offset(offset).Scan(&boards).Error
+
+		if err != nil {
+			return c.JSON(http.StatusOK, &[]Board{})
+		}
+		return c.JSON(http.StatusOK, &boards)
+	}
+}
+
+func UpdatePost(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		body, _ := ioutil.ReadAll(c.Request().Body)
+		req := BoardBody{}
+		json.Unmarshal(body, &req)
+
+		err := db.Debug().Model(&Board{}).Where("id = ?", req.Id).Updates(Board{
+			Title: req.Title,
+			Desc:  req.Desc,
+		}).Error
+		if err != nil {
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				"result": "fail",
+			})
+		}
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"result": "success",
+		})
+	}
+}
+
+func DeletePost(db *gorm.DB) func(echo.Context) error {
+	return func(c echo.Context) error {
+		body, _ := ioutil.ReadAll(c.Request().Body)
+		req := BoardBody{}
+		json.Unmarshal(body, &req)
+
+		err := db.Debug().Delete(&Board{}, req.Id)
+		if err != nil {
+			return c.JSON(http.StatusOK, map[string]interface{}{
+				"result": "fail",
+			})
+		}
+		return c.JSON(http.StatusOK, map[string]interface{}{
+			"result": "success",
+		})
+	}
+}
+
+func main() {
+	DBConnection()
+}
+
+func handlerouting(db *gorm.DB) {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.POST},
+	}))
+
+	e.GET("/", HelloWorld)
+	e.GET("/users/:id", getUser)
+	e.GET("/show", show)
+	e.POST("/save", save)
+	e.POST("/bodypost", BodyPostTest)
+	e.GET("/q1", EachQueryTest)
+	e.GET("/q2", MulQueryTest)
+	e.GET("/p/:num", ParamsTest)
+
+	e2 := e.Group("/api/db")
+
+	e2.POST("/c_post", CreatePost(db))
+	e2.GET("/g_post/:id", GetPostParam(db))
+	e2.GET("/g_qpost", GetPostQuery(db))
+	e2.GET("/g_all", GetAllPost(db))
+	e2.POST("/u_post", UpdatePost(db))
+	e2.POST("/d_post", DeletePost(db))
+	e.Logger.Fatal(e.Start(":8081"))
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module go_echo
+
+go 1.16
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.11 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6 // indirect
+	golang.org/x/sys v0.0.0-20211003122950-b1ebd4e1001c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gorm.io/driver/mysql v1.1.2 // indirect
+	gorm.io/gorm v1.21.15 // indirect
+)
